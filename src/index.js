3c4ad150be6846afc6c6870e5dded0a7921c2fc5
buhariaminat <buhariaminat@yahoo.com>
2022-03-24 21:23:34 +0100

    Add each list

--- src/index.js
+++ src/index.js
@@ -1,26 +1,59 @@
 import './style.css';
+class List {
+  constructor (todo) {
+    this.todo = todo;
+  }
+}
 
-const listElement = document.getElementById('lists-todo');
-const lists = [
-  {
-    index: 1,
-    description: 'Wash the dishes',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'complete To Do list',
-    completed: false,
-  },
-];
+class Alltodo {
+  constructor (lists) {
+    this.lists = lists;
+  }
 
-lists.forEach((list) => {
-  const listHTML = `<li class="list">
-<div class="listblock">
-  <input type="checkbox" name="checkbox" />
-  <p>${list.description}</p>
-</div>
-<button class="elipse"><i class="fa fa-ellipsis-v"></i></button>
-</li>`;
-  listElement.insertAdjacentHTML('beforeend', listHTML);
+  addTodo(list){
+    this.lists.push(list)
+    this.save()
+  }
+
+  removeList(list) {
+   const listIndex = this.lists.findIndex((listItem) => listItem === list)
+   this.lists.splice(listIndex, 1);
+   this.save()
+  }
+  save() {
+    localStorage.setItem('lists', JSON.stringify(this.lists));
+  }
+
+  getsave() {
+    return JSON.parse(localStorage.getItems('lists'))
+  }
+
+  showList() {
+    for (let i = 0; i < this.lists.length; i += 1) {
+      document.getElementById('lists-todo').innerHTML += 
+       `<li class="list">
+      <div class="listblock">
+        <input type="checkbox" name="checkbox" />
+        <p>${this.lists[i].todo}</p>
+      </div>
+      <button class="elipse"><i class="fa fa-ellipsis-v"></i></button>
+      </li>`;  
+    }
+  }
+}
+ 
+let listData = new Alltodo(JSON.parse(localStorage.getItem('lists'))??[]);
+listData.showList();
+
+const addList = document.querySelector('input');
+addList.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    if (addList.value !== null) {
+    let list = new List(addList.value);
+    addList.addTodo(list);
+    addList.reset();
+  }
+  }
 });
+
