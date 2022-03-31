c7d50334f729859d0ac57235bbed7c65d1747c81
dewslyse <dewslyse@gmail.com>
2022-03-31 20:57:35 +0000

    Add test add item function

--- src/function.test.js
+++ src/function.test.js
@@ -4,9 +4,17 @@
 
 import { addTodo, removeTodo } from './function.js';
 
+// const fs = require('fs');
+
+let before;
+let after;
+let inputText;
+
 describe('add a number', () => {
-  test('Add one new item to the list', () => {
-    document.body.innerHTML = `<div class="list-container">
+  // beforeAll(() => {
+  // document.body.innerHTML = fs.readFileSync('dist/index.html');
+  document.body.innerHTML = `
+    <div class="list-container">
         <div class="header">
             <h1>Today's To Do</h1>
             <button type="button" id="reload">
@@ -21,43 +29,49 @@ describe('add a number', () => {
             placeholder="Add to your list..."
         />
         <ul class="lists" id="lists-todo">
-        <li class= "list"></li>
         </ul>
         <button class="clear" type="button" id="clear">
             Clear all completed
         </button>
-        </div>`;
+    </div>
+  `;
+  before = document.querySelectorAll('#lists-todo li');
+  inputText = document.getElementById('input-todo');
+  inputText.value = 'Mock Test';
+  addTodo();
+  // });
 
-    const list = document.querySelectorAll('.list');
-    expect(list).toHaveLength(1);
+  test('Add one new item to the list', () => {
+    after = document.querySelectorAll('#lists-todo li');
+    expect(after).toHaveLength((before.length + 1));
   });
 })
 
-describe('remove a number', () => {
-  test('remove todo list', () => {
-    document.body.innerHTML = `<div class="list-container">
-        <div class="header">
-            <h1>Today's To Do</h1>
-            <button type="button" id="reload">
-            <i class="fa fa-refresh"></i>
-            </button>
-        </div>
-        <input
-            type="text"
-            name=" text"
-            class="input-todo"
-            id="input-todo"
-            placeholder="Add to your list..."
-        />
-        <ul class="lists" id="lists-todo"></ul>
-        <li class= "list"></li>
-        <button class="clear" type="button" id="clear">
-            Clear all completed
-        </button>
-        </div>`;
+// describe('remove a number', () => {
+//   test('remove todo list', () => {
+//     document.body.innerHTML = `<div class="list-container">
+//         <div class="header">
+//             <h1>Today's To Do</h1>
+//             <button type="button" id="reload">
+//             <i class="fa fa-refresh"></i>
+//             </button>
+//         </div>
+//         <input
+//             type="text"
+//             name=" text"
+//             class="input-todo"
+//             id="input-todo"
+//             placeholder="Add to your list..."
+//         />
+//         <ul class="lists" id="lists-todo"></ul>
+//         <li class= "list"></li>
+//         <button class="clear" type="button" id="clear">
+//             Clear all completed
+//         </button>
+//         </div>`;
 
-    removeTodo();
-    const list = document.querySelectorAll('.lists');
-    expect(list).toHaveLength(list.length-1);
-  });
-});
+//     removeTodo();
+//     const list = document.querySelectorAll('.lists');
+//     expect(list).toHaveLength(list.length-1);
+//   });
+// });
