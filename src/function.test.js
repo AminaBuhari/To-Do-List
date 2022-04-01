89e7027c719a4d90cb7de7dc46fa2878bd4315ce
buhariaminat <buhariaminat@yahoo.com>
2022-04-01 14:16:01 +0100

    Chech edit functionality

--- src/function.test.js
+++ src/function.test.js
@@ -2,36 +2,37 @@
  * @jest-environment jsdom
  */
 
-import { addTodo, removeTodo } from './function.js';
+import { addTodo, removeTodo, editTodo } from './function.js';
 
 let before;
 let after;
 let inputText;
+const ourHtml = `
+<div class="list-container">
+    <div class="header">
+        <h1>Today's To Do</h1>
+        <button type="button" id="reload">
+        <i class="fa fa-refresh"></i>
+        </button>
+    </div>
+    <input
+        type="text"
+        name=" text"
+        class="input-todo"
+        id="input-todo"
+        placeholder="Add to your list..."
+    />
+    <ul class="lists" id="lists-todo">
+    </ul>
+    <button class="clear" type="button" id="clear">
+        Clear all completed
+    </button>
+</div>
+`;
 
 describe('add a number', () => {
   beforeAll(() => {
-    document.body.innerHTML = `
-    <div class="list-container">
-        <div class="header">
-            <h1>Today's To Do</h1>
-            <button type="button" id="reload">
-            <i class="fa fa-refresh"></i>
-            </button>
-        </div>
-        <input
-            type="text"
-            name=" text"
-            class="input-todo"
-            id="input-todo"
-            placeholder="Add to your list..."
-        />
-        <ul class="lists" id="lists-todo">
-        </ul>
-        <button class="clear" type="button" id="clear">
-            Clear all completed
-        </button>
-    </div>
-  `;
+    document.body.innerHTML = ourHtml;
     before = document.querySelectorAll('#lists-todo li');
     inputText = document.getElementById('input-todo');
     inputText.value = 'Mock Test';
@@ -51,28 +52,7 @@ describe('add a number', () => {
 
 describe('delete a number', () => {
   beforeAll(() => {
-    document.body.innerHTML = `
-    <div class="list-container">
-        <div class="header">
-            <h1>Today's To Do</h1>
-            <button type="button" id="reload">
-            <i class="fa fa-refresh"></i>
-            </button>
-        </div>
-        <input
-            type="text"
-            name=" text"
-            class="input-todo"
-            id="input-todo"
-            placeholder="Add to your list..."
-        />
-        <ul class="lists" id="lists-todo">
-        </ul>
-        <button class="clear" type="button" id="clear">
-            Clear all completed
-        </button>
-    </div>
-  `;
+    document.body.innerHTML = ourHtml;
     inputText = document.getElementById('input-todo');
     inputText.value = 'Mock Test';
   });
@@ -91,5 +71,22 @@ describe('delete a number', () => {
   test('Remove one item from Local Storage', () => {
     const arr = JSON.parse(localStorage.addlists);
     expect(arr).toHaveLength((before.length - 1));
+  });
+});
+
+describe('Edit the todo', () => {
+  document.innerHTML = ourHtml; 
+  beforeAll(() => {
+    document.body.innerHTML = ourHtml;
+    inputText = document.getElementById('input-todo');
+    inputText.value = 'Mock Test';
+    addTodo();
+    const arr = JSON.parse(localStorage.addlists);
+    editTodo(arr[0]);
+  });
+
+  test('edit the test', () => {
+    const arr = JSON.parse(localStorage.addlists);
+    expect(arr[0].description).toEqual(inputText.value);
   });
 });
